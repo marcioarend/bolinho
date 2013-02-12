eeaafa93adc90152f63d72468fee0de5f7e91e99
Marcio Arend <marcioarend@gmail.com>
2013-02-12 15:05:34 +0100

    Comeco

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bolinho::Application.config.secret_token = '4493206f3e38c31efb59c0f8c3360b6d73c41655b4aa866d17f1d6deb8c563a85e1b189fc2d220cb9720cbba407959c63e066960a8e8af27b3899b1dd037a51a'
